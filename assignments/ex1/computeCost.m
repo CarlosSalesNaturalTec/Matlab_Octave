5ed08a807216482a3d29a8e61fa4b5960aab518e
Carlos Sales <carlossalesti@gmail.com>
2020-06-21 18:16:22 -0300

    semana 3 - OK

--- assignments/ex1/computeCost.m
+++ assignments/ex1/computeCost.m
@@ -12,13 +12,11 @@ J = 0;
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
+;
+predictions = X * theta;                % Valores preditos, dados os parâmetros thetha. (Matriz-Vetor multiplicação)
+sqrErrors   = (predictions - y).^2;     % erros em relação aos dados de treinamento 
 
-theta_0 = theta(1,1);
-theta_1 = theta(2,1);
-
-hypothesis = (theta_0 + (theta_1 * X(:,2) ));
-sum_error_square = sum((hypothesis - y).^2);
-J = ((2*m)^-1) * sum_error_square;
+J = 1/(2*m) * sum(sqrErrors);
 
 % =========================================================================
 
